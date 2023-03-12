5c6a058793fe0e627f4f5ee4d88db3184f2a4f8c
Richard Simpson <richardsimpson@outlook.com>
2023-03-12 18:08:25 -0500

    fix: use package.json for version

--- python/rpdk/typescript/__init__.py
+++ python/rpdk/typescript/__init__.py
@@ -1,5 +1,11 @@
 import logging
+from importlib.resources import files
+import json
 
-VERSION = "2.0.0a0"
+__package_json__ = json.loads(
+    files("rpdk.typescript").joinpath("package.json").read_text()
+)
+
+__version__ = __package_json__["version"].replace("-", "")
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
---
+++ scripts/sync-package.mts
@@ -0,0 +1,3 @@
+import { copy } from "fs-extra";
+
+copy("package.json", "python/rpdk/typescript/package.json");
